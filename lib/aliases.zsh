6f2d525bbab348152186d9649e3d974afdf8bf87
Lars Moelleken <lars@moelleken.org>
2014-09-13 22:20:35 +0200

    [+]: colorize commands if possible

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -65,3 +65,76 @@ alias la='ls -lAh'
 
 alias afind='ack-grep -iH'
 
+# command with color
+if [ "$TERM" != "dumb" ]; then
+
+  # dircolors
+  if [ -s "$HOME/.dircolors" ]; then
+    eval "$(dircolors -b $HOME/.dircolors)"
+  else
+    eval "$(dircolors -b)"
+  fi
+
+  # Use "colordiff" or "highlight" to colour diffs.
+  if command -v colordiff > /dev/null; then
+    alias difflight='colordiff | less -XFIRd'
+  elif command -v highlight > /dev/null; then
+    alias difflight='highlight --dark-red ^-.* | highlight --dark-green ^+.* | highlight --yellow ^Only.* | highlight --yellow ^Files.*differ$ | less -XFIRd'
+  else
+    alias difflight='less -XFIRd'
+  fi
+
+  # colorize these commands if possible
+  if which grc >/dev/null 2>&1; then
+    alias colour='grc -es --colour=auto'
+    alias configure='colour ./configure'
+    alias diff='colour diff'
+    alias make='colour make'
+    alias gcc='colour gcc'
+    alias g++='colour g++'
+    alias ld='colour ld'
+    alias netstat='colour netstat'
+    alias ping='colour ping'
+    alias traceroute='colour traceroute'
+    alias tail='colour tail'
+    alias ps='colour ps'
+    alias syslog='sudo colour tail -f -n 100 /var/log/syslog'
+  fi
+
+  # replace top with htop
+  if command -v htop >/dev/null; then
+    alias top_orig='/usr/bin/top'
+    alias top='htop'
+  fi
+
+  # Colorize the grep command output for ease of use (good for log files)
+  alias grep='grep --color=auto'
+  alias egrep='egrep --color=auto'
+  alias fgrep='fgrep --color=auto'
+
+  # list all files colorized in long format
+  alias l="ls -lhF --color=auto"
+  # Show hidden files
+  alias l.="ls -dlhF .* --color=auto"
+  # use colors
+  alias ls="ls -F --color=auto"
+  # displays all files and directories in detail
+  alias la="ls -laFh --color=auto"
+  # displays all files and directories in detail with newest-files at bottom
+  alias lr="ls -laFhtr --color=auto"
+  # show last 10 recently changed files
+  alias lt="ls -altr | grep -v '^d' | tail -n 10"
+  # show files and directories (also in sub-dir) that was touched in the last hour
+  alias lf='find ./* -ctime -1 | xargs ls -ltr --color'
+  # displays files and directories in detail
+  alias ll="ls -lFh --color=auto --group-directories-first"
+  # shows the most recently modified files at the bottom of
+  alias llr="ls -lartFh --color=auto --group-directories-first"
+  # list only directories
+  alias lsd="ls -lFh --color=auto | grep --color=never '^d'"
+  # displays files and directories
+  alias dir="ls --color=auto --format=vertical"
+  # displays more information about files and directories
+  alias vdir="ls --color=auto --format=long"
+fi
+
