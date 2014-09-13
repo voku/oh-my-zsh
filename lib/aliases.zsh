afdab18e7b90fce56b3e0c3d6a86030d44076fe8
Lars Moelleken <lars@moelleken.org>
2014-09-13 22:06:34 +0200

    [~]: fixed code-style for "lib/aliases.zsh" v2

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -17,11 +17,11 @@ alias _='sudo'
 alias please='sudo'
 
 # Show history
-if [ "$HIST_STAMPS" = "mm/dd/yyyy" ]: then
+if [ "$HIST_STAMPS" = "mm/dd/yyyy" ]; then
     alias history='fc -fl 1'
-elif [ "$HIST_STAMPS" = "dd.mm.yyyy" ]: then
+elif [ "$HIST_STAMPS" = "dd.mm.yyyy" ]; then
     alias history='fc -El 1'
-elif [ "$HIST_STAMPS" = "yyyy-mm-dd" ]: then
+elif [ "$HIST_STAMPS" = "yyyy-mm-dd" ]; then
     alias history='fc -il 1'
 else
     alias history='fc -l 1'
