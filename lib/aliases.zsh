8b892a3e77f43c79223177ba1eb33727a8eb43ec
Lars Moelleken <lars@moelleken.org>
2014-09-13 22:50:34 +0200

    [+]: alias for cpu-info

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -192,3 +192,12 @@ alias meminfo='free -m -l -t'
 # get top process eating memory
 alias psmem='ps auxf | sort -nr -k 4'
 alias psmem10='ps auxf | sort -nr -k 4 | head -10'
+
+# get top process eating cpu
+alias pscpu='ps auxf | sort -nr -k 3'
+alias pscpu10='ps auxf | sort -nr -k 3 | head -10'
+
+# Get server cpu info
+alias cpuinfo='lscpu'
+# older system use /proc/cpuinfo
+#alias cpuinfo='less /proc/cpuinfo'
